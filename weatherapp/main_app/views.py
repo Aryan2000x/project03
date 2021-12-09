4f98360f56c239f1b7c934338fde8dbc4da20b74
aryan ahmed <aryan_ahmed21@aryans-MacBook-Air.local>
2021-12-09 15:54:22 -0500

    add templates for about page

--- weatherapp/main_app/views.py
+++ weatherapp/main_app/views.py
@@ -6,4 +6,4 @@ def home(request):
   return HttpResponse('<h1>Home Page</h1>')
 
 def about(request):
-    return HttpResponse('<h1>Welcome to About Page</h1>')
+    return render(request, 'about.html')
