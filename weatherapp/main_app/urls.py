9fbf6e46a74fb9661cd73e3d75fdb06330d055ac
aryan ahmed <aryan_ahmed21@aryans-MacBook-Air.local>
2021-12-16 21:47:27 -0500

    change url path

--- weatherapp/main_app/urls.py
+++ weatherapp/main_app/urls.py
@@ -5,6 +5,5 @@ urlpatterns = [
     path('', views.index),
     path('accounts/signup/', views.signup, name='signup'),
     # refer to https://medium.com/@frfahim/django-registration-with-confirmation-email-bb5da011e4ef
-    path(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-        views.activate, name='activate'),
+    path('activate/<uidb64>/<token>', views.activate, name='activate'),
 ]
