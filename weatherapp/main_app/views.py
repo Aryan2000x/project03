a113e653d573fde289d8305d29c4dfbe8c1afc95
faisamusse <Faisa.a.musse@gmail.com>
2021-12-16 19:17:12 -0500

    features

--- weatherapp/main_app/views.py
+++ weatherapp/main_app/views.py
@@ -1,30 +1,34 @@
 import requests
 from django.shortcuts import render
+from .models import City
 
 
 # Define the home view
 
-<<<<<<< HEAD
-def about(request):
-    return render(request, 'about.html')
-=======
 def index(request):
   url= "https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=615565f33a31c50af502c7ff0810f560"
 
   city = 'Toronto'
 
-  r = requests.get(url.format(city)).json()
+  cities = City.objects.all()
 
-  city_weather = {
-    'city': city ,
-    'temperature': r['main']['temp'],
-    'description' : r ['weather'][0]['description'],
-    'icon': r['weather'][0]['icon'],
-  }
-  
-  context = {'city_weather': city_weather}
+  weather_data = []
+   
+  for city in cities:
+
+      r = requests.get(url.format(city)).json()
+
+      city_weather = {
+          'city': city.name ,
+          'temperature': r['main']['temp'],
+          'description' : r ['weather'][0]['description'],
+          'icon': r['weather'][0]['icon'],
+      }
+
+      weather_data.append(city_weather)
+
+  print(weather_data)
+
+  context = {'weather_data': weather_data}
 
   return render(request, 'weather/weather.html', context)
-    
-
->>>>>>> master
